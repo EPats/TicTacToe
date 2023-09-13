172e2ac917eba7d8a3971c6bc36b0599d0fd756f
EriottosanMC <94922833+EPats@users.noreply.github.com>
2023-09-13 20:57:09 +0100

    Basic turn implementation

--- main.py
+++ main.py
@@ -9,3 +9,27 @@ empty_board = [[" ", " ", " "] for _ in range(3)]
 
 # Display the empty board
 display_board(empty_board)
+
+# Function to make a move on the board
+def make_move(board, row, col, player):
+    if board[row][col] == " ":
+        board[row][col] = player
+        return True
+    else:
+        print("Cell is already occupied. Choose another cell.")
+        return False
+
+# Test the function
+board = [[" ", " ", " "] for _ in range(3)]
+
+# Player 'X' makes a move at (0, 0)
+make_move(board, 0, 0, 'X')
+
+# Player 'O' makes a move at (1, 1)
+make_move(board, 1, 1, 'O')
+
+# Player 'X' tries to make a move at (0, 0) again
+make_move(board, 0, 0, 'X')
+
+# Display the updated board
+display_board(board)
